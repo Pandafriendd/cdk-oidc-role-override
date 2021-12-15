c77339f7ba84ef3539b7fba07e0b69fc1ae1e2d0
EC2 Default User <ec2-user@ip-172-31-16-141.us-east-2.compute.internal>
2021-12-15 00:18:52 +0000

    first commit

--- lib/cdk-oidc-ts-stack.ts
+++ lib/cdk-oidc-ts-stack.ts
@@ -1,15 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkOidcTsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkOidcTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  
+    
+    const provider = new iam.OpenIdConnectProvider(this, 'Provider', {
+      url: 'https://exmaple.com',
+    });
+    
+    const childCR = this.node.tryFindChild('Custom::AWSCDKOpenIdConnectProviderCustomResourceProvider') as unknown as cdk.CustomResourceProvider;
+    const childRole = childCR.node.tryFindChild('Role') as cdk.CfnResource;
+    childRole.addOverride("Properties.RoleName", "test_role_name");
+  
+    
   }
 }
